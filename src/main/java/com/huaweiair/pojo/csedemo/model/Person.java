77ea476086e5177c60281bf635891578ed171ecf
paas <paas@cse-codegen-backend-1592449050-pkkb5>
2017-11-08 07:50:36 +0000

    Auto pushed by FusionStage!

--- src/main/java/com/huaweiair/pojo/csedemo/model/Person.java
+++ src/main/java/com/huaweiair/pojo/csedemo/model/Person.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import javax.validation.constraints.*;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T07:39:09.558Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T07:50:36.626Z")
 
 public class Person   {
   @JsonProperty("name")
